8fef9e368a11676dc79d1255ac987fbe5cfd0ed9
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2025-07-20 23:48:44 +0100

    add the wxflows

--- src/lib/langgraph.ts
+++ src/lib/langgraph.ts
@@ -1,7 +1,30 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import wxflows from "@wxflows/sdk/langchain"
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { MessagesAnnotation, StateGraph } from "@langchain/langgraph"
+import  SYSTEM_MESSAGE from "../constants/systemMessage"
+import { SystemMessage, trimMessages } from "@langchain/core/messages";
+import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts"
+ 
+const toolClient = new wxflows({
+ endpoint: process.env.WXFLOWS_ENDPOINT || "",
+ apikey : process.env.WXFLOWS_APIKEY
+})
+
+const tools = await toolClient.lcTools;
+const toolNode = new ToolNode(tools)
+
+const trimmers = trimMessages({
+  maxTokens: 10,
+  strategy: "last",
+  tokenCounter: (msgs) => msgs.length,
+  includeSystem: true,
+  allowPartial: false,
+  startOn: "human",
+});
 
 
-export const initialModel = () => {
+const initialModel = () => {
  const model = new ChatGoogleGenerativeAI({
   model : "gemini-2.0-flash",
   apiKey: process.env.GEMENI_API_KEY,
@@ -18,5 +41,19 @@ export const initialModel = () => {
     }
    }
   ]
- })
+ }).bindTools(tools)
+ return model;
 }
+
+const createWorkflow = () => {
+ const model = initialModel();
+ return new StateGraph(MessagesAnnotation).addNode("agent", async (state) => {
+  const systemContent = SYSTEM_MESSAGE;
+  const promptTemplate = ChatPromptTemplate.fromMessages([
+   new SystemMessage(systemContent, {
+    cache_controle : {type : "ephemeral"}
+   }) ,
+   new MessagesPlaceholder("messages")
+  ])
+  })
+ }
